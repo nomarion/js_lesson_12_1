378f2ce9ba0461acf5afecd77858d7a16b31ad82
nomarion <macmini@MBP-Igor.netis.cc>
2025-07-12 08:51:50 +0300

    Lesson 12.1

---
+++ main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let inputUrl;
+const buttonInput = createButton('Введите урл', setInputUrl)
+const buttonRedirect = createButton('Перейти по ссылке', redirect)
+
+document.body.append(buttonInput, buttonRedirect);
+
+function createButton(text, func) {
+    const button = document.createElement('button');
+    button.textContent = text;
+    button.classList.add('button');
+    button.onclick = func;
+    return button;
+}
+
+function setInputUrl() {
+    const input = prompt('Введите урл');
+    if(input !== null && input.trim() !== '') {
+        inputUrl = input;
+        return;
+    }
+    alert('Повторите попытку!');
+}
+
+function redirect() {
+    if(inputUrl !== null && inputUrl !== undefined) {
+        window.location.href = inputUrl;
+    }
+    alert('Укажите ссылку!');
+}
+
